72df98b24849152ab0eb8bea7e4881d5463bd57c
i.krysov <zombie7321@gmail.com>
2021-10-12 14:21:21 +0400

    fixes styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
-import {
-  BrowserRouter as Router, Route, Switch,
-} from 'react-router-dom';
+// import {
+//   BrowserRouter as Router, Route, Switch,
+// } from 'react-router-dom';
+import Main from './pages/Main';
 import routes from './utils/routes';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Switch>
-          {routes.map(({
-            id, exact, path, component,
-          }) => (
-            <Route
-              key={id}
-              exact={exact}
-              id={id}
-              path={path}
-              component={component}
-            />
-          ))}
-        </Switch>
-      </Router>
+      <Main />
+      {/* <Router> */}
+      {/*  <Switch> */}
+      {/*    {routes.map(({ */}
+      {/*      id, exact, path, component, */}
+      {/*    }) => ( */}
+      {/*      <Route */}
+      {/*        key={id} */}
+      {/*        exact={exact} */}
+      {/*        id={id} */}
+      {/*        path={path} */}
+      {/*        component={component} */}
+      {/*      /> */}
+      {/*    ))} */}
+      {/*  </Switch> */}
+      {/* </Router> */}
     </div>
   );
 }
