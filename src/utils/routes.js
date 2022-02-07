7fa5bd7910b3b3ddb8094e6071e9db1b2e7b864c
Ivan <zombie7321@gmail.com>
2022-02-07 18:15:28 +0400

    fixes

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,8 +1,4 @@
 import Main from '../pages/Main';
-import Auth from '../pages/Auth';
-import Admin from '../pages/Admin';
-import Settings from '../pages/Admin/Settings';
-import Mail from '../pages/Admin/Mail';
 import {
   MAIN_ROUTE, AUTH_ROUTE, ADMIN_ROUTE, SETTINGS_ROUTE, MAIL_ROUTE,
 } from './constants';
@@ -13,26 +9,6 @@ const routes = [
     path: MAIN_ROUTE,
     component: Main,
     exact: true,
-  },
-  {
-    id: 2,
-    path: AUTH_ROUTE,
-    component: Auth,
-  },
-  {
-    id: 3,
-    path: ADMIN_ROUTE,
-    component: Admin,
-  },
-  {
-    id: 4,
-    path: SETTINGS_ROUTE,
-    component: Settings,
-  },
-  {
-    id: 5,
-    path: MAIL_ROUTE,
-    component: Mail,
   },
 ];
 
